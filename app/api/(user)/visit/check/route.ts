cf7086ad227dd6731c512666b8192b1be65ddc64
shon <turayevshahzodbek@gmail.com>
2025-10-30 22:28:53 +0900

    feat: check vercel ip

--- app/api/(user)/visit/check/route.ts
+++ app/api/(user)/visit/check/route.ts
@@ -1,24 +1,27 @@
 import { prisma } from "@/lib/prisma";
+import { geolocation } from "@vercel/functions";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-export async function GET() {
+export async function GET(request: Request) {
   try {
-    const cookieStore = await cookies();
-    const vid = cookieStore.get("VID")?.value ?? "";
-    const check = await prisma.visit_list.findFirst({
-      where: { id: vid },
-    });
-    if (check) {
-      return NextResponse.json({
-        check,
-        status: 200,
-      });
-    } else {
-      return NextResponse.json({
-        check,
-        status: 404,
-      });
-    }
+    const data = geolocation(request);
+    // const cookieStore = await cookies();
+    // const vid = cookieStore.get("VID")?.value ?? "";
+    // const check = await prisma.visit_list.findFirst({
+    //   where: { id: vid },
+    // });
+    // if (check) {
+    //   return NextResponse.json({
+    //     check,
+    //     status: 200,
+    //   });
+    // } else {
+    //   return NextResponse.json({
+    //     check,
+    //     status: 404,
+    //   });
+    // }
+    return NextResponse.json({ data });
   } catch (err) {
     return NextResponse.json({
       status: 500,
